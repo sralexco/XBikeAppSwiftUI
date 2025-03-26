b8b5a2a9c7210ba8936502879387c09344b62897
Alexander Cayetano <sr.alexco@gmail.com>
2025-03-26 12:45:50 -0500

    first commit

---
+++ XBikeAppSwiftUI/XBikeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  XBikeAppSwiftUIApp.swift
+//  XBikeAppSwiftUI
+//
+//  Created by alex on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct XBikeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XBikeAppSwiftUITests/XBikeAppSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  XBikeAppSwiftUITests.swift
+//  XBikeAppSwiftUITests
+//
+//  Created by alex on 26/03/25.
+//
+
+import Testing
+@testable import XBikeAppSwiftUI
+
+struct XBikeAppSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
