264152baa977eaa1ca3d549d045ec90134b7ed69
Alexander Cayetano <sr.alexco@gmail.com>
2025-03-28 01:33:54 -0500

    finished the RideSwiftData

---
+++ XBikeAppSwiftUI/Modules/CurrentRide/Model/RideSDModel.swift
@@ -0,0 +1,24 @@
+//
+//  Ride.swift
+//  XBikeAppSwiftUI
+//
+//  Created by alex on 28/03/25.
+//
+
+import SwiftData
+
+@Model
+class RideSDModel {
+    var finalTime: String
+    var startAddress: String
+    var endAddress: String
+    var distance: String
+    
+    init(finalTime: String, startAddress: String, endAddress: String, distance: String) {
+        self.finalTime = finalTime
+        self.startAddress = startAddress
+        self.endAddress = endAddress
+        self.distance = distance
+    }
+}
+    
